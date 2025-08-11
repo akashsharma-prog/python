4b7e6dc0968821c27489927b9564ff4a70f3f8e4
Akash sharma <kalkamaaakshsharma>
2025-08-11 18:56:42 +0530

    contetion statement.py

--- contetion statment.py
+++ contetion statment.py
@@ -8,13 +8,66 @@
     #print("i will do task b")
 
 
-money = int(input("please give me a money"))
+#money = int(input("please give me a money"))
+#if money >= 1 and money <= 10:
+    #print("Give me more money! for ice cream")
+#elif money >= 11 and money <= 20:
+    #print("Give me 10 more rupees")
+#elif money >= 21 and money <= 30: 
+    #print("Some more!")
+#else:
+    #print("Ha ha, I am happy")
 
-if money <=10:
-    print("give me a more money! for ice - cream")
-elif 10<=20:
-    print("give me a more 10rupees")
-else:
-    print("ha ha i am happy")
+
+
+#money = int(input("please give me a money"))
+#if money >= 1 and money <= 10:
+    #print("Give me more money! for ice cream")
+#elif money >= 11 and money <= 20:#
+    #print("Give me 10 more rupees")
+#elif money >= 21 and money <= 30: 
+    #print("Some more!")
+#else:
+    #print("Ha ha, I am happy")
+
+#money = int(input("please give me a money"))
+#if money >= 1 and money <= 10:
+    #print("Give me more money! for ice cream")
+#elif money >= 11 and money <= 20:
+    #print("Give me 10 more rupees")
+#elif money >= 21 and money <= 30: 
+    #print("Some more!")
+#else:
+   # print("Ha ha, I am happy")
+
+    #print("thats is not good ")
+
+
+#num1 = int(input("plz tell your first num1"))
+#num2 = int(input("plz tell you second num2"))
+
+#if num1>num2:#
+
+    #print("num 1 is greater than num 2")
+#elif num1<num2:
+
+    #print("num 2 is greter then num1 ")
+#else:
+    #print("both the number are same")
+
+gen = input("plz tell your gender as chracter (male or female)")
+if gen == "male":
+    print("good morning sir")
+
+elif gen=="female":
+    print("good morning mam")
+else :
+    print("please fill your gender ")
+
+    
+
+
+
+
 
 
