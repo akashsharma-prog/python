a532eaa5c3c4a8755987e687c7ee18641eab6ff0
Akash sharma <kalkamaaakshsharma>
2025-08-11 16:26:14 +0530

    contetion statment

---
+++ contetion statment.py
@@ -0,0 +1,20 @@
+#if ealse
+
+#a  = 13
+
+#if a > 4:
+    #print("i will do task a ")
+#else:
+    #print("i will do task b")
+
+
+money = int(input("please give me a money"))
+
+if money <=10:
+    print("give me a more money! for ice - cream")
+elif 10<=20:
+    print("give me a more 10rupees")
+else:
+    print("ha ha i am happy")
+
+
---
+++ main.py
@@ -0,0 +1,137 @@
+sher = "akash sharma"
+"""
+sheriyansSchool = "studenet" #camel case
+SheriyanSchool = "student"  #pascal case
+sheriyan_school = "student" #snake case"""
+a = -25
+print(type(a))
+
+b = 58.8
+c = 12/3
+print(type(b))
+print(type(c))
+
+d = 34j
+print(type(d))
+
+st = "1548451589cdshncdsb"
+print(type(st))
+
+a = True
+
+"""string"""
+
+a = "o"
+print(ord(a))
+
+a = 16
+print(chr(a))
+"""indexing"""
+a = "akash"
+print(a[2],a[-3])
+
+"""string slicing"""
+
+a = "akash coder"
+print(a[0:5:1])
+print(a[6:11:1])
+print(a[3:5:1])
+
+"""type conversion"""
+
+a = 12
+a = str(a)
+print(type(a))
+
+a = int(a)# you naver convered string in integesr
+
+print(type(a))
+a = 0
+
+print(bool(a))
+
+#a = "akash sharma"
+#age = "20"
+#print(f"my name is {a} and age is{age}")
+#input
+#age = int(input("what is your age"))
+#print(age)
+#print(type(age))
+
+#name = input("hellow what your name")
+#print(type(name))
+#print(name)
+
+#name = int(input("what is your mobile.no."))
+#age = int(input("what is your age"))
+#print(name)
+#print(age)
+a = 5
+b = 20 
+
+#print(a+b)
+#rint(b-a)
+print(b//a)
+print(b/a)
+print(5**5)
+a = 5
+b = 32
+
+#print(a+b)
+#rint(b-a)
+#print(b//a)
+#print(b/a)
+#print(b%a)
+
+
+#assigment operator
+
+a =  23
+#compund assigment operation
+a = 20
+
+#a +=  20
+#a +=  40
+#a +=  60
+#a -= 20
+#a *=30
+#a %=40
+#a //=50
+#a /=60
+
+#print(a)
+
+
+
+"""comparison operator"""
+a = 12.1
+b = 12
+print(a==b)
+
+print(a!=b)
+
+print(a>b)
+print(a<b)
+
+print(23>=23)
+print(245<=25)
+
+print(ord("a"))
+
+print(ord("b"))
+
+print("a">"b")
+
+print("abc">"Acf")
+
+"""logical operator"""
+
+#print(123> 100 and 34==34 and 45<90 and 12>90)
+#print(12!= 12 or 25==54 or 25<2 or 25>8)
+#print (not(12==12))
+
+print(126>130)#false
+print(456==456)!=(235 ==236)#true
+print(12 < 10or 45 ==56 or 69>70or 15!=13 )#true
+print(True and bool(0))#false
+
