2c15e1cfc4471328a436d17ccca377c8cf493ea4
Akash sharma <kalkamaaakshsharma>
2025-08-13 12:37:07 +0530

    mains.py

--- mains.py
+++ mains.py
@@ -18,54 +18,75 @@
 #n = int(input("which table you want to print"))      
 #for d in range(n,n*10+1,n):
        #print(d)
-A="HELLOW MAFIYA MUNDE"    
-print(len(A))
-for i in range(len(A)):
-       print(A[i])
+#A="HELLOW MAFIYA MUNDE"    
+#print(len(A))
+#for i in range(len(A)):
+       #print(A[i])
 #DIRECT METHOD
-A = "Akash is very cool"
+#A = "Akash is very cool"
 
-for i in A:print(i)
+#for i in A:print(i)
 
 
-for i in range(1,21):
-      if i ==15:
-             continue
-      print(i)
-
+#for i in range(1,21):
+     # if i ==15:
+            # continue
+     # print(i)#
+#
 
 #loop question
-a = int(input("please tell your number "))
+#a = int(input("please tell your number "))
 
-for i in range(1,a+1,):
-       print("hellow world")
-       #question 2
+#for i in range(1,a+1,):
+       #print("hellow world")
+       ##question 2
 
-a = int(input("tell me your number"))
-for i in range(1,a+1):
-       print(i)
+#a = int(input("tell me your number"))
+#for i in range(1,a+1):
+      # print(i)
   #question 3
 #0
-a = int(input("tell me your number"))
-for i in range(a,0,-1):
-       print(i)
+#a = int(input("tell me your number"))
+#for i in range(a,0,-1):
+       #print(i)
 #question 4
 
-n = int(input("tell me your table"))
-for i in range(1,5):
-  print(f"{n}*{i}={n*i}")
+#n = int(input("tell me your table"))
+#for i in range(1,5):
+  #print(f"{n}*{i}={n*i}")
 
 #question 5 
-n = int(input("please tell your number:-"))
-sum = 0
-for i in range(1,n+1):
-      sum = sum +i
-print(f"your sum is {sum}")
+##n = int(input("please tell your number:-"))
+##sum = 0
+##for i in range(1,n+1):
+      ##sum = sum +i
+##print(f"your sum is {sum}")
 
 #question 6
-n = int(input("Please tell your number: "))
-fact = 1
-for i in range(1, n + 1):
-    fact *= i  # multiply step by step
+##n = int(input("Please tell your number: "))
+##fact = 1
+#for i in range(1, n + 1):
+    #fact *= i  # multiply step by step
 
-print(f"Your factorial is {fact}")
+#print(f"Your factorial is {fact}")
+
+
+#question no 7
+#n = int(input("Please tell your number: "))
+#even = 0
+#odd = 0
+
+#for i in range(1, n+1):##
+    #if i % 2 == 0:
+        #even += i
+    #else:
+        #odd += i
+
+#print(f"This is sum of odd and even numbers: odd = {odd}, even = {even}")
+#question 8
+n = int(input("which factor you want:-"))
+for i in range (1,n+1):
+   if n%i == 0:        
+     print(f"your factor of {i} ")
+
+
