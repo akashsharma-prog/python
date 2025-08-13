02458775d8f4c3ccacbd8d4faae4951b828ac388
Akash sharma <kalkamaaakshsharma>
2025-08-13 17:15:59 +0530

    yask.py

--- yask.py
+++ yask.py
@@ -1,5 +1,32 @@
-a = 1
-while a < 11:
-      a = a+1
-print(a)
-  # adding 1 
+#a = 1               # starting value
+#while a < 11:       # loop until a is 10
+    #print(a)
+    #a = a + 1       # increase a by 1
+ # question 2
+#a = 10
+#while a>0:
+   # print(a)
+    #a = a-1#
+    #question 3
+#a = int(input("What number you want to reverse: "))
+
+#while a > 0:
+    #print(a % 10)     # print last digit
+    #a = a // 10       # remove last digit
+#question 2
+#count = 1
+#num = 2
+
+#while count<=10:
+   # print(num)
+    #count = count+1
+    #num = num+2
+    
+
+    # question 3
+count =1
+num = 1 
+while count<= 10:
+        print(num)
+        count = count +1 
+        num = num+1
