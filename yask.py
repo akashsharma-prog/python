596db748ed09d1673be36f44253aa16ddffe0420
Akash sharma <kalkamaaakshsharma>
2025-08-14 10:01:15 +0530

    yask.py

--- yask.py
+++ yask.py
@@ -29,4 +29,45 @@ num = 1
 while count<= 10:
         print(num)
         count = count +1 
-        num = num+1
+        num = num+2
+# question 4
+a = 1
+while a<=5:
+        print("Akash sharma")
+        a = a+1
+
+# question 5
+a = int(input("please give me a number"))
+b = 1
+while b<=10:
+        print(f"{a}*{b}*{a*b}")
+        b = b+1
+
+        # question 6
+a = int(input("Please give me a number for sum: "))
+sum = 0
+b = 1
+while b <= a:
+    sum = sum + b
+    b = b + 1
+print("Sum is:", sum)
+# question 7
+a = int(input("give me a number"))
+b = 2
+sum = 0
+while b<=a:
+  b = b+2
+sum = sum+b
+print(f"{a}number sum is {sum}")
+
+# question 8 
+a = int(input("Please give me a number: "))
+count = 0
+
+while a > 0:
+    a = a // 10    # remove last digit
+    count += 1     # increase digit counter
+
+print(f"Your number has {count} digits.")
+
+      
